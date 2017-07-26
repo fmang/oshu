3fce06c7148e1d7c90fa58d40003ae662c84e6bf
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-07-26 21:29:42 +0200

    audio module stub

---
+++ src/audio.c
@@ -0,0 +1,17 @@
+#include "oshu.h"
+
+int oshu_audio_open(const char *url, struct oshu_audio_stream **stream)
+{
+	return 0;
+}
+
+int oshu_audio_play(struct oshu_audio_stream *stream)
+{
+	return 0;
+}
+
+int oshu_audio_close(struct oshu_audio_stream *stream)
+{
+	return 0;
+}
+
--- src/oshu.h
+++ src/oshu.h
@@ -4,3 +4,9 @@
 #define oshu_log_warn(...) SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define oshu_log_error(...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define oshu_log_critical(...) SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
+
+struct oshu_audio_stream {};
+
+int oshu_audio_open(const char *url, struct oshu_audio_stream **stream);
+int oshu_audio_play(struct oshu_audio_stream *stream);
+int oshu_audio_close(struct oshu_audio_stream *stream);
