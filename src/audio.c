e3caca69a08650981045251443b4be7e6ac26e9b
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-07-28 08:08:32 +0200

    audio: reorganize audio_open

--- src/audio.c
+++ src/audio.c
@@ -12,26 +12,42 @@ void oshu_audio_init()
 	av_register_all();
 }
 
+int open_demuxer(const char *url, struct oshu_audio_stream *stream)
+{
+	int rc = avformat_open_input(&stream->demuxer, url, NULL, NULL);
+	if (rc < 0) {
+		oshu_log_error("failed opening the audio file");
+		return rc;
+	}
+	rc = av_find_best_stream(
+		stream->demuxer,
+		AVMEDIA_TYPE_AUDIO,
+		-1, -1,
+		&stream->codec,
+		0
+	);
+	if (rc < 0 || stream->codec == NULL) {
+		oshu_log_error("error finding the best audio stream");
+		return rc;
+	}
+	stream->stream_id = rc;
+	return 0;
+}
+
+int open_decoder(struct oshu_audio_stream *stream)
+{
+	return 0;
+}
+
 int oshu_audio_open(const char *url, struct oshu_audio_stream **stream)
 {
 	*stream = calloc(1, sizeof(**stream));
-	int rc = avformat_open_input(&(*stream)->demuxer, url, NULL, NULL);
-	if (rc < 0) {
-		oshu_log_error("failed opening the audio file");
+	int rc = open_demuxer(url, *stream);
+	if (rc < 0)
 		goto fail;
-	}
-	rc = av_find_best_stream(
-		(*stream)->demuxer,
-		AVMEDIA_TYPE_AUDIO,
-		-1, -1,
-		&(*stream)->codec,
-		0
-	);
-	if (rc < 0 || (*stream)->codec == NULL) {
-		oshu_log_error("error finding the best audio stream");
+	rc = open_decoder(*stream);
+	if (rc < 0)
 		goto fail;
-	}
-	(*stream)->stream_id = rc;
 	return 0;
 fail:
 	log_av_error(rc);
