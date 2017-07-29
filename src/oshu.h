2c0b0608f781a209732bfdbac48bc63c571aa90e
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-07-29 22:24:18 +0200

    move the logging macros into log.h

--- src/oshu.h
+++ src/oshu.h
@@ -1,10 +1,4 @@
 #pragma once
 
-#define oshu_log_verbose(...) SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define oshu_log_debug(...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define oshu_log_info(...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define oshu_log_warn(...) SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define oshu_log_error(...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-#define oshu_log_critical(...) SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
-
+#include "log.h"
 #include "audio.h"
