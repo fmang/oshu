87a25c0707c1acaadb58ca7843adfb27fc412365
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-07-30 15:43:59 +0200

    log: document the use of the error levels

--- src/log.h
+++ src/log.h
@@ -11,11 +11,42 @@
  * @{
  */
 
+/**
+ * Unused so far.
+ */
 #define oshu_log_verbose(...)  SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
+
+/**
+ * Debugging messages are for developers and advanced users. They shouldn't be
+ * shown to everyone, except to inspect the cause of a failure. Use this
+ * freely.
+ */
 #define oshu_log_debug(...)    SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "[" __FILE__ "] " __VA_ARGS__)
+
+/**
+ * Informational messages are things that we'd like most users to see. They
+ * give relevant information about things one might like to know. For example,
+ * information about the audio file, and beatmap metadata.
+ */
 #define oshu_log_info(...)     SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
+
+/**
+ * Warning messages are for non-fatal errors. They'll be the primary indicator
+ * of unimplemented non-essential features.
+ */
 #define oshu_log_warn(...)     SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
+
+/**
+ * Error messages explain errors fatal to the task we were doing. These won't
+ * make the game crash, even though in most cases we'll choose to end the game
+ * anyway. Failure to read a beatmap, or to open an audio file are errors.
+ */
 #define oshu_log_error(...)    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
+
+/**
+ * Critical messages are for desperate cases, when the game has no other option
+ * but to crash. For example, when SDL fails to initialize.
+ */
 #define oshu_log_critical(...) SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 
 /** @} */
