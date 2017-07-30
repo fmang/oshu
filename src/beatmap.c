f14217a70cce3d48b22ccf2c32e60b5e000cdd37
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-07-30 14:52:08 +0200

    document the beatmap module

--- src/beatmap.c
+++ src/beatmap.c
@@ -4,22 +4,49 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Every osu beatmap file must begin with this.
+ *
+ * Beware the first three non-printable bytes!
+ */
 static const char *osu_file_header = "\xef\xbb\xbfosu file format v";
 
+/**
+ * Enumeration to keep track of the section we're currently in.
+ *
+ * This is especially important before the format inside each section changes
+ * significantly, some being INI-like, and others being CSV-like.
+ */
 enum beatmap_section {
-	BEATMAP_HEADER = 0,
-	BEATMAP_ROOT,
-	BEATMAP_GENERAL,
-	BEATMAP_METADATA,
-	BEATMAP_HIT_OBJECTS,
+	BEATMAP_HEADER = 0, /**< Expect the \ref osu_file_header. */
+	BEATMAP_ROOT, /**< Between the header and the first section. We expect nothing. */
+	BEATMAP_GENERAL, /**< INI-like. */
+	BEATMAP_METADATA, /**< INI-like. */
+	BEATMAP_HIT_OBJECTS, /**< CSV-like. */
 	BEATMAP_UNKNOWN,
 };
 
+/**
+ * The parsing process is split into many functions, each minding its own
+ * business.
+ *
+ * To make the prototypes of these functions easy, all they pass in the line to
+ * parse and that parser state structure.
+ *
+ * You'll note the parsing is based on an automaton model, with a transition
+ * function receiving one line at a time, and updating the parser state.
+ */
 struct parser_state {
 	struct oshu_beatmap *beatmap;
 	enum beatmap_section section;
 };
 
+/**
+ * Trim a string.
+ *
+ * The passed pointer is moved to the first non-space character, and the
+ * trailing spaces are replaced with null characters.
+ */
 static void trim(char **str)
 {
 	for (; **str == ' '; (*str)++);
@@ -27,6 +54,16 @@ static void trim(char **str)
 	for (; rev >= *str && *rev == ' '; rev--);
 }
 
+/**
+ * Extract a key-value pair from a line like `key: value`.
+ *
+ * This function receives two pointers to uninitialized pointers, which this
+ * function will make point to valid zero-terminated strings. Both the key and
+ * the value are trimmed using \ref trim.
+ *
+ * If no colon deliminter was found, the whole line is saved into `*key` and
+ * `*value` is set to *NULL*.
+ */
 static void key_value(char *line, char **key, char **value)
 {
 	char *colon = strchr(line, ':');
@@ -43,6 +80,11 @@ static void key_value(char *line, char **key, char **value)
 	}
 }
 
+/**
+ * Parse the first line of the beatmap file and extract the version number.
+ *
+ * Fails if it not a valid header, or if it couldn't parse the version.
+ */
 static int parse_header(char *line, struct parser_state *parser)
 {
 	if (strncmp(line, osu_file_header, strlen(osu_file_header)) == 0) {
@@ -61,6 +103,14 @@ static int parse_header(char *line, struct parser_state *parser)
 	return -1;
 }
 
+/**
+ * Parse a section like `[General]`.
+ *
+ * If the line passed as argument is not a section definition line, or doesn't
+ * end with a closing square bracket, fail.
+ *
+ * Unknown sections are okay.
+ */
 static int parse_section(char *line, struct parser_state *parser)
 {
 	char *section, *end;
@@ -87,6 +137,9 @@ fail:
 	return -1;
 }
 
+/**
+ * Parse a key-value line in the `[General]` section.
+ */
 static int parse_general(char *line, struct parser_state *parser)
 {
 	char *key, *value;
@@ -101,7 +154,12 @@ static int parse_general(char *line, struct parser_state *parser)
 	return 0;
 }
 
-static int parse_one_hit(char *line, struct oshu_hit **hit)
+/**
+ * Allocate and parse one hit object.
+ *
+ * Only failure, `*hit` is set to *NULL*.
+ */
+static void parse_one_hit(char *line, struct oshu_hit **hit)
 {
 	char *x = strsep(&line, ",");
 	char *y = strsep(&line, ",");
@@ -110,21 +168,24 @@ static int parse_one_hit(char *line, struct oshu_hit **hit)
 	if (!type) {
 		oshu_log_warn("invalid hit object");
 		*hit = NULL;
-		return 0;
+		return;
 	}
 	*hit = calloc(1, sizeof(**hit));
 	(*hit)->x = atoi(x);
 	(*hit)->y = atoi(y);
 	(*hit)->time = atoi(time);
 	(*hit)->type = atoi(type);
-	return 0;
 }
 
+/**
+ * Parse one hit using \ref parse_one_hit, and link it into the whole beatmap.
+ *
+ * Skip invalid hit objects.
+ */
 static int parse_hit_object(char *line, struct parser_state *parser)
 {
 	struct oshu_hit *hit;
-	if (parse_one_hit(line, &hit) < 0)
-		return -1;
+	parse_one_hit(line, &hit);
 	if (hit) {
 		if (!parser->beatmap->hits)
 			parser->beatmap->hits = hit;
@@ -137,11 +198,17 @@ static int parse_hit_object(char *line, struct parser_state *parser)
 	return 0;
 }
 
+/**
+ * The main parser transition function.
+ *
+ * Dispatch the current line according to the parser state.
+ *
+ * Trim the lines, and skip comments and empty lines.
+ */
 static int parse_line(char *line, struct parser_state *parser)
 {
 	int rc = 0;
-	/* skip spaces */
-	for (; *line == ' '; line++);
+	trim(&line);
 	if (*line == '\0') {
 		/* skip empty lines */
 	} else if (line[0] == '/' && line[1] == '/') {
@@ -158,6 +225,10 @@ static int parse_line(char *line, struct parser_state *parser)
 	return rc;
 }
 
+/**
+ * Create the parser state, then read the input file line-by-line, feeding it
+ * to the parser automaton with \ref parse_line.
+ */
 static int parse_file(FILE *input, struct oshu_beatmap *beatmap)
 {
 	struct parser_state parser;
--- src/beatmap.h
+++ src/beatmap.h
@@ -5,42 +5,77 @@
  * Check there for the format reference:
  * https://osu.ppy.sh/help/wiki/osu!_File_Formats
  *
+ * In particular this one:
+ * https://osu.ppy.sh/help/wiki/osu!_File_Formats/Osu_(file_format)
+ *
  * To find sample files, just download any beatmap you find on the official
  * Osu! website. The .osz files are just ZIP files. After you extract them,
  * you'll find the .osu files we're going to parse in this module.
+ *
+ * An *.osu* file is some kind of pseudo-ini, with sections written like
+ * `[Metadata]` and key values, except they're written `key: value`. Some
+ * sections are completely unlike INI, but pretty something like CSV.
+ *
+ * Because the format is so chaotic, the parser will happily ignore things it
+ * doesn't understand, and generate a few warnings.
  *
  * @{
  */
 
 enum oshu_mode {
 	OSHU_MODE_OSU = 0,
+	OSHU_MODE_TAIKO = 1,
+	OSHU_MODE_CATCH_THE_BEAT = 2,
+	OSHU_MODE_MANIA = 3,
 };
 
+/**
+ * One timing point.
+ *
+ * This structure is a complete transcription of the timing point description
+ * in the documentation, even though it's not used yet.
+ */
 struct oshu_timing_point {
-	int offset; /* ms */
+	int offset; /**< Milliseconds. */
 	float ms_per_beat;
 	int meter;
 	int sample_type;
 	int sample_set;
-	int volume; /* 0-100 */
+	int volume; /**< 0-100. */
 	int inherited;
 	int kiai;
 };
 
+/**
+ * Flags defining the type of a hit object.
+ *
+ * To check if it's a circle, you should use `type & OSHU_HIT_CIRCLE` rather
+ * than check for equality, because it will often be combined with \ref
+ * OSHU_HIT_NEW_COMBO.
+ */
 enum oshu_hit_type {
 	OSHU_HIT_CIRCLE = 0b1,
 	OSHU_HIT_SLIDER = 0b10,
 	OSHU_HIT_NEW_COMBO = 0b100,
 	OSHU_HIT_SPINNER = 0b1000,
-	OSHU_HIT_COMBO_MASK = 0b1110000,
-	OSHU_HIT_HOLD = 0b10000000,
+	OSHU_HIT_COMBO_MASK = 0b1110000, /**< How many combos to skip. */
+	OSHU_HIT_HOLD = 0b10000000, /**< Mania mode only. */
 };
 
+/**
+ * One hit object, and one link in the beatmap.
+ *
+ * This structure actually defines a link in the linked list of all hit
+ * objects, sorted by chronological order.
+ *
+ * It is currently very limited, because it doesn't support sliders, or the
+ * sample set.
+ */
 struct oshu_hit {
-	int x; /* 0-512 */
-	int y; /* 0-384 */
-	int time; /* ms */
-	int type;
+	int x; /**< 0-512 pixels, inclusive. */
+	int y; /**< 0-384 pixels, inclusive. */
+	int time; /**< Milliseconds. */
+	int type; /**< Combination of flags from \ref oshu_hit_type. */
 	struct oshu_hit *next;
 };
 
@@ -66,8 +101,22 @@ struct oshu_beatmap {
 	struct oshu_hit *hit_cursor;
 };
 
+/**
+ * Take a path to a *.osu* file, open it and parse it.
+ *
+ * The parsed beatmap is returned by setting `*beatmap` to point to a newly
+ * allocated beatmap object.
+ *
+ * On failure, the beatmap is automatically free'd.
+ */
 int oshu_beatmap_load(const char *path, struct oshu_beatmap **beatmap);
 
+/**
+ * Free the beatmap object and every other dynamically allocated object it
+ * refers to.
+ *
+ * Sets `*beatmap` to *NULL.
+ */
 void oshu_beatmap_free(struct oshu_beatmap **beatmap);
 
 /** @} */
