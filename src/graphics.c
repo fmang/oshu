5054fa965690b13305cf544a33b6e826053489bb
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-07-31 21:31:29 +0200

    add a margin around the window

--- src/graphics.c
+++ src/graphics.c
@@ -5,6 +5,9 @@
 static const int window_width = 640; /* px */
 static const int window_height = 480; /* px */
 
+static const int game_width = 512;
+static const int game_height = 384;
+
 static const int hit_radius = 32; /* px */
 
 int create_window(struct oshu_display *display)
@@ -59,17 +62,23 @@ void oshu_display_destroy(struct oshu_display **display)
 	*display = NULL;
 }
 
+void draw_hit(struct oshu_display *display, struct oshu_hit *hit)
+{
+	int horizontal_margin = (window_width - game_width) / 2;
+	int vertical_margin = (window_height - game_height) / 2;
+	SDL_Rect where;
+	where.x = horizontal_margin + hit->x - hit_radius;
+	where.y = vertical_margin + hit->y - hit_radius;
+	where.w = hit_radius * 2;
+	where.h = hit_radius * 2;
+	SDL_RenderCopy(display->renderer, display->hit_mark, NULL, &where);
+}
+
 void oshu_draw_beatmap(struct oshu_display *display, struct oshu_beatmap *beatmap, int msecs)
 {
 	SDL_SetRenderDrawColor(display->renderer, 0, 0, 0, 255);
 	SDL_RenderClear(display->renderer);
-	if (beatmap->hit_cursor) {
-		SDL_Rect where;
-		where.x = beatmap->hit_cursor->x - hit_radius;
-		where.y = beatmap->hit_cursor->y - hit_radius;
-		where.w = hit_radius * 2;
-		where.h = hit_radius * 2;
-		SDL_RenderCopy(display->renderer, display->hit_mark, NULL, &where);
-	}
+	if (beatmap->hit_cursor)
+		draw_hit(display, beatmap->hit_cursor);
 	SDL_RenderPresent(display->renderer);
 }
