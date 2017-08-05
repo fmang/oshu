0f827e3849c949714b8af38ea1afee09a36c82f9
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-08-05 13:52:33 +0200

    game module structure

---
+++ src/game.c
@@ -0,0 +1,6 @@
+/**
+ * \file game.c
+ * \ingroup game
+ *
+ * \ref game module implementation.
+ */
---
+++ src/game.h
@@ -0,0 +1,48 @@
+/**
+ * \file game.h
+ * \ingroup game
+ *
+ * \ref game module definition.
+ */
+
+#pragma once
+
+#include "audio.h"
+#include "beatmap.h"
+#include "graphics.h"
+
+/**
+ * \defgroup game Game
+ *
+ * \brief
+ * Coordinate all the modules and implement the game mechanics.
+ *
+ * \{
+ */
+
+/**
+ * The full game state, from the beatmap state to the audio and graphical
+ * context.
+ */
+struct oshu_game {
+	struct oshu_beatmap *beatmap;
+	struct oshu_audio *audio;
+	struct oshu_display *display;
+};
+
+/**
+ * Create the game context for a beatmap, and load all the associated assets.
+ */
+int oshu_game_create(const char *beatmap_path, struct oshu_game **game);
+
+/**
+ * Free the memory for everything, and set `*game` to *NULL*.
+ */
+int oshu_game_destroy(struct oshu_game **game);
+
+/**
+ * Start the main event loop.
+ */
+int oshu_game_run(struct oshu_game *game);
+
+/** \} */
