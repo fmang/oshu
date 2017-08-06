b57330529c059b2c1c5b70660403d6e8ed59ec47
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-08-06 18:42:51 +0200

    polish the audio sample submodule

--- src/audio.c
+++ src/audio.c
@@ -408,10 +408,17 @@ static int convert_audio(SDL_AudioSpec *device_spec, SDL_AudioSpec *wav_spec, st
 		return -1;
 	}
 	sample->buffer = realloc(sample->buffer, sample->length * converter.len_mult);
+	if (sample->buffer == NULL) {
+		oshu_log_error("could not resize the sample buffer");
+		return -1;
+	}
 	converter.buf = sample->buffer;
 	converter.len = sample->length;
 	SDL_ConvertAudio(&converter);
 	sample->length = converter.len_cvt;
+	/* reclaim the unrequired memory */
+	sample->buffer = realloc(sample->buffer, sample->length);
+	assert (sample->buffer != NULL);
 	return 0;
 }
 
@@ -419,21 +426,29 @@ int oshu_sample_load(const char *path, struct oshu_audio *stream, struct oshu_sa
 {
 	SDL_AudioSpec spec;
 	*sample = calloc(1, sizeof(**sample));
+	if (*sample == NULL) {
+		oshu_log_error("could not allocate the memory for the sample object");
+		return -1;
+	}
 	SDL_AudioSpec *wav = SDL_LoadWAV(path, &spec, &(*sample)->buffer, &(*sample)->length);
-	if (wav == NULL)
+	if (wav == NULL) {
+		oshu_log_error("SDL error when loading sample: %s", SDL_GetError());
 		goto fail;
+	}
 	if (convert_audio(&stream->device_spec, wav, *sample) < 0)
 		goto fail;
 	return 0;
 fail:
-	oshu_log_error("SDL error when loading sample: %s", SDL_GetError());
-	free(*sample);
-	*sample = NULL;
+	oshu_sample_free(sample);
 	return -1;
 }
 
 void oshu_sample_free(struct oshu_sample **sample)
 {
-	SDL_FreeWAV((*sample)->buffer);
+	if (*sample == NULL)
+		return;
+	if ((*sample)->buffer)
+		SDL_FreeWAV((*sample)->buffer);
+	free(*sample);
 	*sample = NULL;
 }
