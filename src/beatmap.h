94310a3244adec633f4184f7f79aa6d9eea2c3cd
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-08-12 12:16:29 +0200

    beatmap.h: timing points & more

--- src/beatmap.h
+++ src/beatmap.h
@@ -43,21 +43,98 @@ enum oshu_mode {
 	OSHU_MODE_MANIA = 3,
 };
 
+enum oshu_sampleset_category {
+	OSHU_SAMPLESET_NORMAL,
+	OSHU_SAMPLESET_SOFT,
+	OSHU_SAMPLESET_DRUM,
+};
+
+enum oshu_sample_type {
+	OSHU_SAMPLE_TYPE_NORMAL,
+	OSHU_SAMPLE_TYPE_WHISTLE,
+	OSHU_SAMPLE_TYPE_FISNISH,
+	OSHU_SAMPLE_TYPE_CLAP,
+};
+
 /**
  * One timing point.
  *
  * This structure is a complete transcription of the timing point description
  * in the documentation, even though it's not used yet.
+ *
+ * Here's an example: `66,315.789473684211,4,2,0,45,1,0`.
+ *
+ * The fields of this structures are in the same order as these field-separated
+ * values.
+ *
+ * Some timing points are inherited, and have a negative milleseconds per beat.
+ * I guess they should have the inherited flag set too but that's not always
+ * the case it seems. These points require us to keep a pointer to the last
+ * non-inherited timing point.
+ *
+ * The structure provides a #next field to make a linked list *in chronological
+ * order* with respect to #offset.
  */
 struct oshu_timing_point {
-	int offset; /**< Milliseconds. */
-	float ms_per_beat;
+	/**
+	 * \brief When the timing point starts, in seconds.
+	 *
+	 * It's an integral number of milliseconds in the beatmap file.
+	 */
+	double offset;
+	/**
+	 * \brief Duration of a beat in seconds.
+	 *
+	 * In the beatmap file, it's a float of the milliseconds per beat.
+	 *
+	 * When negative, it probably means we should substract the absolute
+	 * value of the raw field to that of the previous non-negative timing
+	 * point. The parser's job should compute the actual value and store it
+	 * here in seconds.
+	 */
+	double beat_duration;
+	/**
+	 * \brief Number of beats in a measure.
+	 */
 	int meter;
-	int sample_type;
-	int sample_set;
-	int volume; /**< 0-100. */
+	/**
+	 * Default sample type to use in that timing section.
+	 */
+	enum oshu_sample_type sample_type;
+	/**
+	 * Default sample set to use in that timing section.
+	 */
+	enum oshu_sampleset_category sample_set;
+	/**
+	 * Volume of the samples, from 0 to 100%.
+	 */
+	int volume;
+	/**
+	 * Pseudo-boolean telling whether the timing point was inherited or
+	 * not. It's looks redundant with the negative #beat_duration.
+	 *
+	 * Looking at a random beatmap file, I found tons of negative beat
+	 * durations but not a single inherited flag. Maybe this flag should
+	 * make the parser reuse the sample set, type and volume of the
+	 * previous hit point?
+	 */
 	int inherited;
+	/**
+	 * Kiai mode. I guess it's when everything flashes on the screen.
+	 *
+	 * It's a pseudo-boolean, and its position in the timing point line is
+	 * unclear. Something tells me it's possibly before the #inherited
+	 * field.
+	 */
 	int kiai;
+	/**
+	 * Pointer to the next timing point.
+	 *
+	 * The next point must have a greater #offset.
+	 *
+	 * NULL for the last item.
+	 */
+	struct oshu_timing_point *next;
 };
 
 /**
@@ -92,11 +169,46 @@ enum oshu_hit_state {
  * sample set.
  */
 struct oshu_hit {
-	int x; /**< 0-512 pixels, inclusive. */
-	int y; /**< 0-384 pixels, inclusive. */
-	int time; /**< Milliseconds. */
-	int type; /**< Combination of flags from #oshu_hit_type. */
+	/**
+	 * x-coordinate of the position of the hit object.
+	 *
+	 * It ranges from 0 (left) to 512 (right) pixels, inclusive.
+	 */
+	int x;
+	/**
+	 * y-coordinate of the position of the hit object.
+	 *
+	 * It ranges from 0 (top) to 384 (bottom) pixels, inclusive.
+	 */
+	int y;
+	/**
+	 * \brief When the hit object should be clicked, in seconds.
+	 *
+	 * In the beatmap, it's an integral number of milliseconds.
+	 */
+	double time;
+	 /**
+	  * Type of the hit object, like circle, slider, spinner, and a few
+	  * extra information.
+	  *
+	  * Combination of flags from #oshu_hit_type.
+	  */
+	int type;
+	/**
+	 * Dynamic state of the hit. Whether it was clicked or not.
+	 *
+	 * It should be left to 0 (#OSHU_HIT_INITIAL) by the parser.
+	 */
 	enum oshu_hit_state state;
+	/**
+	 * Timing point in effect when the hit object should be clicked.
+	 */
+	struct oshu_timing_point *timing_point;
+	/**
+	 * Pointer to the next element of the linked list.
+	 *
+	 * NULL if it's the last element.
+	 */
 	struct oshu_hit *next;
 };
 
@@ -249,19 +361,7 @@ struct oshu_difficulty {
 	double slider_tick_rate;
 };
 
-struct oshu_event {
-};
-
-struct oshu_timing_point {
-};
-
 struct oshu_color {
-};
-
-enum oshu_sampleset_category {
-	OSHU_SAMPLESET_NORMAL,
-	OSHU_SAMPLESET_SOFT,
-	OSHU_SAMPLESET_DRUM,
 };
 
 /**
@@ -378,11 +478,21 @@ struct oshu_beatmap {
 	 */
 	struct oshu_difficulty difficulty;
 	/**
-	 * \brief [Event] section.
+	 * \brief Path to the background picture.
+	 *
+	 * It is extracted from the [Events] section, with a line looking like
+	 * `0,0,"BG.jpg",0,0`.
+	 *
+	 * The section may contain other effects and break information, but
+	 * until we figure it out, let's keep only this crucial property.
+	 *
+	 * May be NULL.
 	 */
-	struct oshu_event *events;
+	char *background_file;
 	/**
 	 * \brief [TimingPoints] section.
+	 *
+	 * It's a linked list, in chronological order.
 	 */
 	struct oshu_timing_point *timing_points;
 	/**
