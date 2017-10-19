8b1c97c62a92eed323bf0e6551e65fd7d6c614fa
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-10-19 20:58:11 +0200

    define the game keys

--- src/game/mode.h
+++ src/game/mode.h
@@ -18,6 +18,53 @@
 struct oshu_game;
 
 /**
+ * The various keys and buttons.
+ *
+ * This makes the intention clearer, as we're identifying fingers rather than
+ * letters. The translation is made in the common game module.
+ *
+ * See https://osu.ppy.sh/help/wiki/shared/Mania_key_layouts.jpg
+ * and more specially https://osu.ppy.sh/help/wiki/shared/Mania_key_layouts.jpg
+ *
+ * The alternative keys, on the right side of the picture, won't be supported
+ * until I figure out how you're supposed to play with them.
+ *
+ * The osu! keys are Z and X, mapped to the left middle finger and left index,
+ * respectively. Note that it's one-handed because the right hand is busy with
+ * the mouse.
+ *
+ * The taiko keys are Z, X, C, V, mapped to left middle finger, index, right
+ * index, middle finger, respectively.
+ *
+ * Because the keys' positions are crucial, the keyboard keys should be
+ * detected in a layout-agnostic way, from the physical codes.
+ */
+enum oshu_key {
+	OSHU_LEFT_PINKY = -4,
+	OSHU_LEFT_RING = -3,
+	OSHU_LEFT_MIDDLE = -2,
+	OSHU_LEFT_INDEX = -1,
+	OSHU_THUMBS = 0,
+	OSHU_RIGHT_INDEX = 1,
+	OSHU_RIGHT_MIDDLE = 2,
+	OSHU_RIGHT_RING = 3,
+	OSHU_RIGHT_PINKY = 4,
+	/**
+	 * Playing with the mouse is discouraged, but if you really wanna do
+	 * this, the buttons are mapped to values overlapping those of keys,
+	 * because the mouse will always be a substitute rather than a real
+	 * thing.
+	 *
+	 * The game module, of course, will have access to the original event
+	 * and may do whatever it pleases with it, to navigate menus for
+	 * example. The game modes, however, have no business knowing that.
+	 */
+	OSHU_LEFT_BUTTON = -1,
+	OSHU_MIDDLE_BUTTON = 0,
+	OSHU_RIGHT_BUTTON = 1,
+};
+
+/**
  * A generic mode is defined by a structure of callbacks.
  *
  * All these callbacks take the game state, and return 0 on success, -1 on
