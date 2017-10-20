85b7066ab128966d43824f4fbffe3b726b31b7b4
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-10-20 20:50:23 +0200

    game: tweak the definition of oshu_key

--- src/game/mode.h
+++ src/game/mode.h
@@ -36,6 +36,9 @@ struct oshu_game;
  *
  * Because the keys' positions are crucial, the keyboard keys should be
  * detected in a layout-agnostic way, from the physical codes.
+ *
+ * Keys on the left hand are negative, while keys on the right hand are
+ * positive, making it easy to test which hand a constant belongs to.
  */
 enum oshu_key {
 	OSHU_LEFT_PINKY = -4,
@@ -48,20 +51,31 @@ enum oshu_key {
 	OSHU_RIGHT_RING = 3,
 	OSHU_RIGHT_PINKY = 4,
 	/**
-	 * Playing with the mouse is discouraged, but if you really wanna do
-	 * this, the buttons are mapped to values overlapping those of keys,
-	 * because the mouse will always be a substitute rather than a real
-	 * thing.
+	 * Playing with the mouse is discouraged, but if you really wanna be
+	 * able to catch the clicks, this is for you. In particular, it is nice
+	 * for newcomers, as clicking is more intuitive than pressing X.
 	 *
-	 * The game module, of course, will have access to the original event
-	 * and may do whatever it pleases with it, to navigate menus for
-	 * example. The game modes, however, have no business knowing that.
+	 * The game module has have access to the original event and may do
+	 * whatever it pleases with it, to navigate menus for example. The game
+	 * modes, however, have no business knowing that, and should handle
+	 * clicks like keys.
 	 */
-	OSHU_LEFT_BUTTON = -1,
-	OSHU_MIDDLE_BUTTON = 0,
-	OSHU_RIGHT_BUTTON = 1,
+	OSHU_LEFT_BUTTON = -5,
 	/**
-	 * Special value
+	 * See #OSHU_LEFT_BUTTON.
+	 */
+	OSHU_RIGHT_BUTTON = 5,
+	/**
+	 * Special value when a key couldn't be translated.
+	 *
+	 * 0 and -1 are already taken, so it's some arbitrary negative value,
+	 * which makes it look like it's for the left hand.
+	 *
+	 * Anyway, this key is only meant as a temporary value, and it is never
+	 * passed to the #oshu_game_mode::pressed. No need to make special
+	 * cases for it. However, because new keys might be added in the future
+	 * (who knows?) you should have a sane default case for keys your
+	 * module doesn't handle, if relevant.
 	 */
         OSHU_UNKNOWN_KEY = -100,
 };
