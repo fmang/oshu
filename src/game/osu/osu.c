20de6ede9a88f3377c335ba949bbd252fa368a6f
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-11-01 11:57:59 +0100

    osu: refactor the slider logic

--- src/game/osu/osu.c
+++ src/game/osu/osu.c
@@ -33,103 +33,6 @@ static struct oshu_hit* find_hit(struct oshu_game *game, struct oshu_point p)
 }
 
 /**
- * Check the state of the current slider.
- *
- * Make it end if it's done.
- *
- * \todo
- * This function is too complex, split it.
- */
-static void check_slider(struct oshu_game *game)
-{
-	struct oshu_hit *hit = game->osu.current_slider;
-	if (!hit)
-		return;
-	if (!(hit->type & OSHU_SLIDER_HIT))
-		return;
-	double t = (game->clock.now - hit->time) / hit->slider.duration;
-	double prev_t = (game->clock.before - hit->time) / hit->slider.duration;
-	if (game->clock.now > oshu_hit_end_time(hit)) {
-		oshu_stop_loop(&game->audio);
-		game->osu.current_slider = NULL;
-		hit->state = OSHU_GOOD_HIT;
-		oshu_play_sound(&game->library, &hit->slider.sounds[hit->slider.repeat], &game->audio);
-	} else if ((int) t > (int) prev_t && prev_t > 0) {
-		oshu_play_sound(&game->library, &hit->slider.sounds[(int) t], &game->audio);
-	}
-	if (game->autoplay)
-		return;
-	struct oshu_point ball = oshu_path_at(&hit->slider.path, t);
-	struct oshu_point mouse = oshu_get_mouse(&game->display);
-	double dist = oshu_distance(ball, mouse);
-	if (dist > game->beatmap.difficulty.slider_tolerance) {
-		oshu_stop_loop(&game->audio);
-		game->osu.current_slider = NULL;
-		hit->state = OSHU_MISSED_HIT;
-	}
-}
-
-/**
- * Get the audio position and deduce events from it.
- *
- * For example, when the audio is past a hit object and beyond the threshold of
- * tolerance, mark that hit as missed.
- */
-static void check_audio(struct oshu_game *game)
-{
-	double left_wall = game->clock.now - game->beatmap.difficulty.leniency;
-	while (game->hit_cursor->time < left_wall) {
-		struct oshu_hit *hit = game->hit_cursor;
-		/* Mark dead notes as missed. */
-		if (!(hit->type & (OSHU_CIRCLE_HIT | OSHU_SLIDER_HIT)))
-			hit->state = OSHU_UNKNOWN_HIT;
-		else if (hit->state == OSHU_INITIAL_HIT)
-			hit->state = OSHU_MISSED_HIT;
-		game->hit_cursor = hit->next;
-	}
-}
-
-/**
- * \todo
- * Merge with #check_audio maybe.
- * For sliders, see how it can be factor with autoplay.
- */
-static int check(struct oshu_game *game)
-{
-	check_slider(game);
-	check_audio(game);
-	return 0;
-}
-
-/**
- * \todo
- * Factor the slider logic.
- *
- * \todo
- * Factor the hit actions.
- */
-static int autoplay(struct oshu_game *game)
-{
-	check_slider(game);
-	while (game->hit_cursor->time < game->clock.now) {
-		struct oshu_hit *hit = game->hit_cursor;
-		if (hit->type & OSHU_SLIDER_HIT) {
-			hit->state = OSHU_SLIDING_HIT;
-			game->osu.current_slider = hit;
-			oshu_play_sound(&game->library, &hit->sound, &game->audio);
-			oshu_play_sound(&game->library, &hit->slider.sounds[0], &game->audio);
-		} else if (hit->type & OSHU_CIRCLE_HIT) {
-			hit->state = OSHU_GOOD_HIT;
-			oshu_play_sound(&game->library, &hit->sound, &game->audio);
-		} else {
-			hit->state = OSHU_UNKNOWN_HIT;
-		}
-		game->hit_cursor = hit->next;
-	}
-	return 0;
-}
-
-/**
  * Release the held slider, either because the held key is released, or because
  * a new slider is activated (somehow).
  */
@@ -147,6 +50,89 @@ static void release_slider(struct oshu_game *game)
 	}
 	oshu_stop_loop(&game->audio);
 	game->osu.current_slider = NULL;
+}
+
+/**
+ * Make the sliders emit a sound when reaching an end point.
+ *
+ * When the last one is reached, release the slider.
+ */
+static void sonorize_slider(struct oshu_game *game)
+{
+	struct oshu_hit *hit = game->osu.current_slider;
+	if (!hit)
+		return;
+	assert (hit->type & OSHU_SLIDER_HIT);
+	int t = (game->clock.now - hit->time) / hit->slider.duration;
+	int prev_t = (game->clock.before - hit->time) / hit->slider.duration;
+	if (game->clock.now > oshu_hit_end_time(hit)) {
+		release_slider(game);
+	} else if (t > prev_t && prev_t >= 0) {
+		assert (t <= hit->slider.repeat);
+		oshu_play_sound(&game->library, &hit->slider.sounds[t], &game->audio);
+	}
+}
+
+/**
+ * Get the audio position and deduce events from it.
+ *
+ * For example, when the audio is past a hit object and beyond the threshold of
+ * tolerance, mark that hit as missed.
+ *
+ * Also mark sliders as missed if the mouse is too far from where it's supposed
+ * to be.
+ */
+static int check(struct oshu_game *game)
+{
+	/* Ensure the mouse follows the slider. */
+	sonorize_slider(game); /* < may release the slider! */
+	if (game->osu.current_slider) {
+		struct oshu_hit *hit = game->osu.current_slider;
+		double t = (game->clock.now - hit->time) / hit->slider.duration;
+		struct oshu_point ball = oshu_path_at(&hit->slider.path, t);
+		struct oshu_point mouse = oshu_get_mouse(&game->display);
+		if (oshu_distance(ball, mouse) > game->beatmap.difficulty.slider_tolerance) {
+			oshu_stop_loop(&game->audio);
+			game->osu.current_slider = NULL;
+			hit->state = OSHU_MISSED_HIT;
+		}
+	}
+	/* Mark dead notes as missed. */
+	double left_wall = game->clock.now - game->beatmap.difficulty.leniency;
+	while (game->hit_cursor->time < left_wall) {
+		struct oshu_hit *hit = game->hit_cursor;
+		if (!(hit->type & (OSHU_CIRCLE_HIT | OSHU_SLIDER_HIT)))
+			hit->state = OSHU_UNKNOWN_HIT;
+		else if (hit->state == OSHU_INITIAL_HIT)
+			hit->state = OSHU_MISSED_HIT;
+		game->hit_cursor = hit->next;
+	}
+	return 0;
+}
+
+/**
+ * \todo
+ * Factor the hit actions.
+ */
+static int autoplay(struct oshu_game *game)
+{
+	sonorize_slider(game);
+	while (game->hit_cursor->time < game->clock.now) {
+		struct oshu_hit *hit = game->hit_cursor;
+		if (hit->type & OSHU_SLIDER_HIT) {
+			hit->state = OSHU_SLIDING_HIT;
+			game->osu.current_slider = hit;
+			oshu_play_sound(&game->library, &hit->sound, &game->audio);
+			oshu_play_sound(&game->library, &hit->slider.sounds[0], &game->audio);
+		} else if (hit->type & OSHU_CIRCLE_HIT) {
+			hit->state = OSHU_GOOD_HIT;
+			oshu_play_sound(&game->library, &hit->sound, &game->audio);
+		} else {
+			hit->state = OSHU_UNKNOWN_HIT;
+		}
+		game->hit_cursor = hit->next;
+	}
+	return 0;
 }
 
 /**
