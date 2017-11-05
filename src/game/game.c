2d0a87521f8911fea2e5611999390357d51da1a5
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-11-05 10:26:05 +0100

    comlexe the scancode to finger translation

--- src/game/game.c
+++ src/game/game.c
@@ -122,8 +122,20 @@ static void forward_music(struct oshu_game *game, double offset)
 enum oshu_key translate_key(SDL_Keysym *keysym)
 {
 	switch (keysym->scancode) {
+	/* Bottom row, for standard and taiko modes. */
 	case SDL_SCANCODE_Z: return OSHU_LEFT_MIDDLE;
 	case SDL_SCANCODE_X: return OSHU_LEFT_INDEX;
+	case SDL_SCANCODE_C: return OSHU_RIGHT_INDEX;
+	case SDL_SCANCODE_V: return OSHU_RIGHT_MIDDLE;
+	/* Middle row, for mania. */
+	case SDL_SCANCODE_A: return OSHU_LEFT_PINKY;
+	case SDL_SCANCODE_S: return OSHU_LEFT_RING;
+	case SDL_SCANCODE_D: return OSHU_LEFT_MIDDLE;
+	case SDL_SCANCODE_F: return OSHU_LEFT_INDEX;
+	case SDL_SCANCODE_J: return OSHU_RIGHT_INDEX;
+	case SDL_SCANCODE_K: return OSHU_RIGHT_MIDDLE;
+	case SDL_SCANCODE_L: return OSHU_RIGHT_RING;
+	case SDL_SCANCODE_SEMICOLON: return OSHU_RIGHT_PINKY;
 	default:             return OSHU_UNKNOWN_KEY;
 	}
 }
