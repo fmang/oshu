de5c04acb56d8d9c9cdffbf4c69bf2914d415025
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-11-13 20:06:25 +0100

    paint with relative sizes

--- src/game/osu/paint.c
+++ src/game/osu/paint.c
@@ -6,24 +6,30 @@
 #include "game/game.h"
 #include "graphics/paint.h"
 
+#include "log.h"
+
 static int paint_circle(struct oshu_game *game)
 {
+	double radius = game->beatmap.difficulty.circle_radius;
+	oshu_size size = (1. + I) *  radius * 2.;
+	double zoom = 3;
+
 	struct oshu_painter p;
-	oshu_start_painting(256 + 256 * I, &p);
-	cairo_translate(p.cr, 128, 128);
+	oshu_start_painting(size * zoom, &p);
+	cairo_translate(p.cr, radius * zoom, radius * zoom);
 
 	cairo_set_source_rgba(p.cr, 1, 1, 1, .7);
-	cairo_arc(p.cr, 0, 0, 128, 0, 2. * M_PI);
+	cairo_arc(p.cr, 0, 0, radius * zoom, 0, 2. * M_PI);
 	cairo_fill(p.cr);
 
 	cairo_set_source_rgba(p.cr, .1, .1, .1, 1);
-	cairo_set_line_width(p.cr, 10);
-	cairo_arc(p.cr, 0, 0, 118, 0, 2. * M_PI);
+	cairo_set_line_width(p.cr, 3 * zoom);
+	cairo_arc(p.cr, 0, 0, (radius - 4) * zoom, 0, 2. * M_PI);
 	cairo_stroke(p.cr);
 
 	int rc = oshu_finish_painting(&p, &game->display, &game->osu.circle_texture);
-	game->osu.circle_texture.size = (1. + I) * game->beatmap.difficulty.circle_radius * 2.;
-	game->osu.circle_texture.origin = game->osu.circle_texture.size / 2.;
+	game->osu.circle_texture.size = size;
+	game->osu.circle_texture.origin = size / 2.;
 	return rc;
 }
 
