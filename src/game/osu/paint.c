7145c1f3b07e5f2652588e04745529657cc5ca23
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2017-11-13 21:07:58 +0100

    let cairo scale for high resolutions

--- src/game/osu/paint.c
+++ src/game/osu/paint.c
@@ -16,15 +16,16 @@ static int paint_circle(struct oshu_game *game)
 
 	struct oshu_painter p;
 	oshu_start_painting(size * zoom, &p);
-	cairo_translate(p.cr, radius * zoom, radius * zoom);
+	cairo_scale(p.cr, zoom, zoom);
+	cairo_translate(p.cr, radius, radius);
 
 	cairo_set_source_rgba(p.cr, 1, 1, 1, .7);
-	cairo_arc(p.cr, 0, 0, radius * zoom, 0, 2. * M_PI);
+	cairo_arc(p.cr, 0, 0, radius, 0, 2. * M_PI);
 	cairo_fill(p.cr);
 
 	cairo_set_source_rgba(p.cr, .1, .1, .1, 1);
-	cairo_set_line_width(p.cr, 3 * zoom);
-	cairo_arc(p.cr, 0, 0, (radius - 4) * zoom, 0, 2. * M_PI);
+	cairo_set_line_width(p.cr, 3);
+	cairo_arc(p.cr, 0, 0, radius - 4, 0, 2. * M_PI);
 	cairo_stroke(p.cr);
 
 	int rc = oshu_finish_painting(&p, &game->display, &game->osu.circle_texture);
