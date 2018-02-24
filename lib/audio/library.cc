08037c08ed4a1b6636326474c28d6f795d44007d
Frédéric Mangano-Tarumi <fmang@mg0.fr>
2018-02-24 19:04:33 +0100

    audio: use the minimal skin for now
    The full skin isn't downloaded anymore. Until skin management is ready,
    let's fallback on this.

--- lib/audio/library.cc
+++ lib/audio/library.cc
@@ -1,6 +1,11 @@
 /**
  * \file audio/library.cc
  * \ingroup audio_library
+ *
+ * \todo
+ * This whole sub-module should not be in the audio module, but deserves to be
+ * in a dedicated *skin* module. *audio* then won't depend on *beatmap*
+ * anymore.
  */
 
 #include "config.h"
@@ -219,7 +224,7 @@ static char* locate_sample(enum oshu_sample_set_family set, int index, int type)
 	} else {
 		/* Check the installation's data directory. */
 		char *path;
-		int rc = asprintf(&path, "%s/samples/%s", PKGDATADIR, filename);
+		int rc = asprintf(&path, "%s/skins/minimal/%s", PKGDATADIR, filename);
 		assert (rc >= 0);
 		if (access(path, R_OK) == 0)
 			return path;
